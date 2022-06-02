e54fe1a7b8c5148f411d3be1b3d27020bdc8985d
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-02 16:58:20 -0400

    Initial commit

---
+++ src/main/java/me/rallings/javaspringbootjparest/JavaSpringbootJparestApplication.java
@@ -0,0 +1,13 @@
+package me.rallings.javaspringbootjparest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootJparestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootJparestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/rallings/javaspringbootjparest/JavaSpringbootJparestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rallings.javaspringbootjparest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootJparestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
