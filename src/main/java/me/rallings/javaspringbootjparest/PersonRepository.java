8064b5b30cdc259494f087bb13005b27351731a0
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-02 16:59:55 -0400

    Created repository (that extends PagingAndSortingRepository) to manage the person-objects

---
+++ src/main/java/me/rallings/javaspringbootjparest/PersonRepository.java
@@ -0,0 +1,19 @@
+package me.rallings.javaspringbootjparest;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+//Extends the PagingAndSortingRepository interface
+//that allows you to perform various tasks and operations
+// on the person objects
+//implementation is automatically created at runtime
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+
+}
